324da3b2da60617c2b037cd475c0f17dde6d0acb
RachelChung001 <84513264+RachelChung001@users.noreply.github.com>
2021-05-26 17:40:49 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,90 @@
+info.onCountdownEnd(function () {
+    game.over(true)
+})
+info.onLifeZero(function () {
+    game.over(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeLifeBy(1)
+    music.powerUp.play()
+    mySprite2.setPosition(-30, -30)
+    pause(2000)
+    mySprite2.setPosition(randint(39, 120), randint(40, 100))
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    music.powerDown.play()
+    info.changeLifeBy(-1)
+    pause(1000)
+})
+let mySprite2: Sprite = null
+scene.setBackgroundColor(9)
+info.setLife(3)
+game.showLongText("Don't get tagged! Apples will heal you. Survive for 30 seconds to win!", DialogLayout.Bottom)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b c . . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    b d d d b b d 5 5 5 4 4 4 4 4 b 
+    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+    c d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c b d d c c b 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setStayInScreen(true)
+mySprite.setPosition(28, 26)
+let myEnemy = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .......fb111111ffff.....
+    ......fffcdb1bc111cf....
+    ....fc111cbfbf1b1b1f....
+    ....f1b1b1ffffbfbfbf....
+    ....fbfbfffffff.........
+    .........fffff..........
+    ..........fff...........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Enemy)
+myEnemy.follow(mySprite, 82)
+info.startCountdown(30)
+mySprite2 = sprites.create(img`
+    . . . . . . . e c 7 . . . . . . 
+    . . . . e e e c 7 7 e e . . . . 
+    . . c e e e e c 7 e 2 2 e e . . 
+    . c e e e e e c 6 e e 2 2 2 e . 
+    . c e e e 2 e c c 2 4 5 4 2 e . 
+    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+    . . . 2 2 e e 4 4 4 2 e e . . . 
+    . . . . . 2 2 e e e e . . . . . 
+    `, SpriteKind.Food)
+mySprite2.setPosition(randint(39, 120), randint(40, 100))
